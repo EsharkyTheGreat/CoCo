deba4fbcc0475a000a52bd4c8bceb89625ee5789
Bhardwaj Bhaskar <bhaskar0120@gmail.com>
2023-08-19 02:35:15 +0530

    Basic working model

--- b.py
+++ b.py
@@ -8,21 +8,29 @@ class Banner:
     failed_login = "\033[0;31mUnable to Login\033[0m"
     failed_keyword = "\033[0;31mInvalid keyword\033[0m"
     Help = "Help coming soon!"
+    welcome = "Welcome to Collector Codes!!\nYour invite code is"
 
-keywords = {"add":2, "mark":2, "list":1, "leaderboard":1 , "help":1 , "q":1}
-def add(token,q):
-    x = requests.post(url+"/add", json={"token":token,"question":q}).json()
+from requests import get,post
+url = 'http://192.168.203.204:5000'
 
-def list(token,q):
-    x = requests.post(url+"/list").json()
+def add(token,chunks):
+    x = post(url+"/add", json={"token":token,"question":chunks[1]}).json()
+    print(x)
 
-def leaderboard(token,q):
-    x = requests.get(url+"/leaderboard").json()
+def qlist(token,chunks):
+    x = get(url+"/list", json={"token":token}).json()
+    print(x)
 
-def mark(token,q):
-    x = requests.post(url+"/mark", json={"token":token,"question":q}).json()
+def leaderboard(token,chunks):
+    x = get(url+"/leaderboard", json={"token":token}).json()
+    print(x)
+
+def mark(token,chunks):
+    x = post(url+"/mark", json={"token":token,"question":chunks[1]}).json()
+    print(x)
 
 
+keywords = {"add":2, "mark":2, "list":1, "leaderboard":1 , "help":1 , "q":1, }
 keyword_to_func = {"add":add, "mark":mark, "list":qlist, "leaderboard":leaderboard}
 
 def session(token):
@@ -31,38 +39,37 @@ def session(token):
         chunks = inst.split()
         if (chunks[0] not in keywords) or (len(chunks) != keywords[chunks[0]]):
             print(Banner.failed_keyword)
+            continue
         if(chunks[0] == 'q'): exit(0)
-        elif(chunks[1] == 'help'):
+        elif(chunks[0] == 'help'):
             print(Banner.Help)
+            continue
+        keyword_to_func[chunks[0]](token,chunks)
 
-
-
-
-
-
-
-from requests import get,post
 def login():
     username = input(Banner.username)
     password = input(Banner.password)
-    x = requests.post(url+"/login", json={"username":username,"password":password}).json()
-    if x.status != "ok": 
+    x = post(url+"/login", json={"username":username,"password":password}).json()
+    if x['status'] != "ok": 
+        print("\033[031;m", x['message'], "\033[031;m", sep="")
         print(Banner.failed_login)
         return False
-    session(x.token)
+    session(x['token'])
 
 def autologin(username,password):
-    x = requests.post(url+"/login", json={"username":username,"password":password}).json()
-    return x.token
+    x =post(url+"/login", json={"username":username,"password":password}).json()
+    return x['token']
 
 def register():
-    invite = input(Banner.invite)
+    invite = int(input(Banner.invite))
     username = input(Banner.username)
     password = input(Banner.password)
-    x = requests.post(url+"/register", json={"username":username,"password":password,"invite":invite}).json()
-    if x.status != "ok": 
+    x = post(url+"/register", json={"username":username,"password":password,"invite":invite}).json()
+    if x['status'] != "ok": 
+        print("\033[031;m", x['message'], "\033[031;m",sep="")
         print(Banner.failed_login)
         return False
+    print(Banner.welcome,'\033[0;32m', x['invite'], '\033[0m')
     token = autologin(username,password)
     session(token)
 
